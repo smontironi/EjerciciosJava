43600e3ff2daf368103d0959b044d2953e321b65
Seba <Seba@192.168.0.92>
2021-05-06 12:31:57 -0300

    Ejercicio 5 del capítulo 1

---
+++ Ejercicio0105/src/ejercicio0105/Ejercicio0105.java
@@ -0,0 +1,34 @@
+/*
+ * Pediremos do snotas enteras y calcularemos la media. Con la media puede
+ * tener decimales utilizaremos una viariable de tipo real.
+ */
+package ejercicio0105;
+import java.util.Scanner;
+/**
+ *
+ * @author Seba
+ */
+public class Ejercicio0105 {
+
+    public static void main(String[] args) {
+        int nota1, nota2;   //variables enteras para las notas
+        double media;       //la media puede contener decimales: usamos double
+        Scanner sc = new Scanner(System.in);
+        
+        //leemos la notas
+        System.out.println("Nota 1: ");
+        nota1 = sc.nextInt();
+        System.out.println("Nota 2: ");
+        nota2 = sc.nextInt();
+        
+        //calculamos la media
+        media = (nota1 + nota2) / 2.0;
+        // en la operacion (nota1 + nota2) / 2.0 , el punto del 2 hace que 
+        // no sea una divison entera. Con ello el resultado subre una 
+        // conversion automatica a real en doble precision y conserva
+        // la parte decimal.
+        
+        System.out.println("La media es: " + media);
+    }
+    
+}
