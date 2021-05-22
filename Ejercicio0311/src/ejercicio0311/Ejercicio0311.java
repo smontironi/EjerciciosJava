26d3f289da9ec65578e416afb4f689842a353faf
Seba <Seba@192.168.0.92>
2021-05-22 16:38:19 -0300

    Ejercicio 11 del capítulo 3

---
+++ Ejercicio0311/src/ejercicio0311/Ejercicio0311.java
@@ -0,0 +1,28 @@
+/*
+Ya tenemos un algoritmo (en el ejercicio anterior) para realizar la tabla de multiplicar 
+de un numero dado. La idea es aprovecharlo, yejecutar el codigo repetidas 
+veces para mostrar las tablas de mulitplicar del 1 al 10.
+ */
+package ejercicio0311;
+
+/**
+ *
+ * @author Seba
+ */
+public class Ejercicio0311 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (int tabla = 1; tabla <= 10; tabla++) {
+            System.out.println("\n\nTabla del " + tabla);
+			
+            //por cada iteracion del bucle enterior, el interior se ejecuta 10 veces
+            for (int i = 1; i<=10; i++){
+		System.out.println(tabla + " x " + i  + " = " + tabla * i);
+	}
+			}
+    }
+    
+}
